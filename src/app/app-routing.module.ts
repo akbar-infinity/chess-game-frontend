c91192d161f386dabcaba4a5cba67db07a5af192
Akbar Shaikh <akbarshaikh.2020@gmail.com>
2021-04-07 09:13:38 +0530

    route updated with path param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'randomid',
+    path: ':gameId',
     component: ChessBoardComponent
   }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  gameId: string;
+
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  generateRandomId() {
+    // generate random string and assign that string to gameId variable
+
+  }
+
+  newGame() {
+    console.log('new game clicked');
+    this.router.navigate([`${this.gameId}`]);
+  }
+
+  joinGame() {
+    console.log('join game clicked');
+    this.router.navigate([`${this.gameId}`]);
   }
 
 }
