9741fb5d133619f5bfa62f087f43755c32be7210
Akbar Shaikh <akbarshaikh.2020@gmail.com>
2021-04-08 09:51:37 +0530

    get game id from backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,27 +11,29 @@ export class HomeComponent implements OnInit {
 
   gameId: string;
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, public http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  generateRandomId() {
-    // generate random string and assign that string to gameId variable
-    let gameId = Math.random().toString(36).substring(7);
-    console.log("random", gameId);
-    return gameId;
-
+  getGameId(): string {
+    this.http.get('http://localhost:4001/getUniqueGameId').subscribe((response) => {
+      console.log('response : ', response);
+      if(response['status']) {
+        this.gameId = response['gameId'];
+      }
+    })
+    return '';
   }
 
   newGame() {
-    console.log('new game clicked');
-    this.router.navigate([`${this.generateRandomId()}`]);
+    console.log('new game clicked', this.getGameId());
+    // this.router.navigate([`${this.generateRandomId()}`]);
   }
 
   joinGame() {
     console.log('join game clicked');
-    this.router.navigate([`${this.generateRandomId()}`]);
+    this.router.navigate([`${this.getGameId()}`]);
   }
 
 }
